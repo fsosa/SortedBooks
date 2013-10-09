b4300e97d0c79bb7f93b30294e3eed048ddda2c9
Fidel Sosa <fidel@fidelsosa.com>
2013-10-08 22:34:29 -0400

    Initial Commit

---
+++ Library/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Library
+//
+//  Created by Fidel Sosa on 10/8/13.
+//  Copyright (c) 2013 Fidel Sosa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
